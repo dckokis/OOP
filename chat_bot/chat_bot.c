975b1a04f42262c173f81cb9be70ab89cf56583c
Konstantin Berengolts <63435062+dckokis@users.noreply.github.com>
2021-03-04 21:02:32 +0700

    Started.

---
+++ chat_bot/chat_bot.c
@@ -0,0 +1,5 @@
+//
+// Created by константин on 04.03.2021.
+//
+
+#include "chat_bot.h"
---
+++ chat_bot/chat_bot.h
@@ -0,0 +1,8 @@
+//
+// Created by константин on 04.03.2021.
+//
+
+#ifndef CHAT_BOT_CHAT_BOT_H
+#define CHAT_BOT_CHAT_BOT_H
+
+#endif //CHAT_BOT_CHAT_BOT_H
