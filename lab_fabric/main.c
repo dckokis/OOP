2712bfafbdb8cc477224591a720d21e1bffd8b19
Konstantin Berengolts <63435062+dckokis@users.noreply.github.com>
2021-03-15 18:01:55 +0700

    It works. Need some reviews.

--- lab_fabric/main.c
+++ lab_fabric/main.c
@@ -1,14 +1,11 @@
 #include "draw_object.h"
-#include <string.h>
 #include <stdio.h>
 
 
 int main(int argc, char **argv) {
     char file_name[40] = "INPUT.txt";
-    //strcpy(file_name, *++argv);
     FILE* input = fopen(file_name, "r");
     draw_object(input);
     fclose(input);
-    //getchar();
     return 0;
 }
