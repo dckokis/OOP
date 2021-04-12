d3b61b12a6cc7947414717836879626ee917ef21
Konstantin Berengolts <63435062+dckokis@users.noreply.github.com>
2021-04-12 23:16:47 +0700

    Все надо переделать

--- lab_calendar/calendar.hpp
+++ lab_calendar/calendar.hpp
@@ -5,33 +5,8 @@
 #include <vector>
 #include "parser.hpp"
 
-namespace {
-    enum FORMAT {
-        HORIZONTAL = 1,
-        VERTICAL
-    };
-}
+
 
 class Calendar {
-public:
-    Calendar(std::vector<std::string> input);
-
-    void Draw(std::string input);
-
-    [[nodiscard]] inline int Year() const {
-        return _year;
-    }
-
-    [[nodiscard]] inline FORMAT Format() const{
-        return _format;
-    }
-private:
-    void DrawCalendarMonth(std::string input);
-
-    void DrawCalendarYear(std::string input);
-
-    void DrawCalendarRange(std::string input);
-
-    FORMAT _format = HORIZONTAL;
-    int _year;
+    Calendar();
 };
--- lab_calendar/parser.cpp
+++ lab_calendar/parser.cpp
@@ -1,5 +1,1 @@
-//
-// Created by константин on 12.04.2021.
-//
-
 #include "parser.hpp"
